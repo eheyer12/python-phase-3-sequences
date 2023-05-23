ed95cb42d97e9f512faffc1f4c0379d0575a8e6f
Eduardo Heyer <eduardo.heyer12@gmail.com>
2023-05-23 10:17:33 -0400

    completed fibonacci sequence lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    if length >= 1:
+        sequence.append(0)
+    if length >= 2:
+        sequence.append(1)
+    for i in range(2, length):
+        next_number = sequence[i - 1] + sequence[i - 2]
+        sequence.append(next_number)
+        
+    print(sequence)
